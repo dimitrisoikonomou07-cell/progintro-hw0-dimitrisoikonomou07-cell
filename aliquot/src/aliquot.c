16d807bc0c4f6c2c004f04d5ed2bd2d3d5ebcf7d
doikonomou <dimitrisoikonomou07@gmail.com>
2025-12-10 22:22:42 +0200

    Created an alliquot function

--- aliquot/src/aliquot.c
+++ aliquot/src/aliquot.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <math.h>
+
+int aliquot_term(int n){
+    int sum = 0;
+    for (int i=1; i<=n/2; i++)
+    {
+        if (n%i==0)
+        {
+            
+            if (n!=i)
+             
+            {
+                sum = sum + i;
+            }
+        }
+    }
+     
+    return sum;
+
+
+}
 
 int main(){
 
-    printf("aliquot\n");
+    int sum = 0;  
+    int n = 0;
 
+    scanf("%d",&n);
+
+    sum = aliquot_term(n);
+
+    printf("%d\n",sum);
+    return 0;
 }
